939859de4dc0c931a78f38c5f6fe42395689b5a1
lucaskbr <lucas_klasa@hotmail.com>
2020-04-21 22:32:26 -0300

    test: add favoriteRepository service test

--- src/favorite-repositories/tests/favorite-repositories.service.spec.ts
+++ src/favorite-repositories/tests/favorite-repositories.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FavoriteRepositoriesService } from '../favorite-repositories.service';
+import { FavoriteRepository } from '../favorite-repositories.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const favoriteRepositoriesMock = [
+  new FavoriteRepository('test1', 'testAvatar1', 'testUrl1'),
+  new FavoriteRepository('test2', 'testAvatar2', 'testUrl2'),
+  new FavoriteRepository('test3', 'testAvatar3', 'testUrl3'),
+];
 
 describe('FavoriteRepositoriesService', () => {
   let service: FavoriteRepositoriesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FavoriteRepositoriesService],
+      providers: [
+        FavoriteRepositoriesService,
+        {
+          provide: getRepositoryToken(FavoriteRepository),
+          useValue: {
+            find: jest.fn().mockResolvedValue(favoriteRepositoriesMock),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<FavoriteRepositoriesService>(
@@ -14,7 +30,10 @@ describe('FavoriteRepositoriesService', () => {
     );
   });
 
-  it('should be defined', () => {
-    //expect(service).toBeDefined();
+  describe('FindAll', () => {
+    it('should return an array of favoriteRepositories', async () => {
+      const result = await service.findAll();
+      expect(result).toEqual(favoriteRepositoriesMock);
+    });
   });
 });
